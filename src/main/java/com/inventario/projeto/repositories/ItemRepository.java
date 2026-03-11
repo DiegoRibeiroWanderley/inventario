b55198a7ae8c2cabf5cfd62f1008ccbcc4bff101
Diego <eliwandejp@gmail.com>
2026-03-11 18:22:38 -0300

    feat(repository): Criar repositório para persistência

---
+++ src/main/java/com/inventario/projeto/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.inventario.projeto.repositories;
+
+import com.inventario.projeto.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+}
