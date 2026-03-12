31aa8e27fcd54adb3829e03234ddfd8b866f824a
Diego <eliwandejp@gmail.com>
2026-03-12 19:38:44 -0300

    feat: Adicionar mapper para Categoria

---
+++ src/main/java/com/inventario/projeto/mapper/CategoriaMapper.java
@@ -0,0 +1,15 @@
+package com.inventario.projeto.mapper;
+
+import com.inventario.projeto.DTOs.CategoriaDTO;
+import com.inventario.projeto.model.Categoria;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CategoriaMapper {
+
+    CategoriaDTO toCategoriaDTO(Categoria categoria);
+    Categoria toCategoria(CategoriaDTO categoriaDTO);
+    List<CategoriaDTO> toCategoriaDTOs(List<Categoria> categorias);
+}
