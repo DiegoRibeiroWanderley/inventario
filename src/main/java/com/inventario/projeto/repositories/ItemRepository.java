b16b35d2b5e2144dd23f49163ca9d5f4b6ea119e
Diego <eliwandejp@gmail.com>
2026-03-15 16:58:36 -0300

    refactor: Retirar JpaSpecificationExecutor<Item> do ItemRepository

--- src/main/java/com/inventario/projeto/repositories/ItemRepository.java
+++ src/main/java/com/inventario/projeto/repositories/ItemRepository.java
@@ -1,11 +1,9 @@
 package com.inventario.projeto.repositories;
 
 import com.inventario.projeto.model.Item;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends JpaRepository<Item, Integer>, JpaSpecificationExecutor<Item> {
+public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
