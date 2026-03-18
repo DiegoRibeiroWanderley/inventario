b71e3c7efbc1029e268da920ae79cee5cdc20fae
Diego <eliwandejp@gmail.com>
2026-03-17 21:10:47 -0300

    feat: Criar enum para os status da Entrega em Status

---
+++ src/main/java/com/inventario/projeto/model/enums/Status.java
@@ -0,0 +1,10 @@
+package com.inventario.projeto.model.enums;
+
+public enum Status {
+    PEDIDO_RECEBIDO,
+    PAGAMENTO_APROVADO,
+    SEPARANDO_PRODUTOS,
+    DESPACHADO,
+    EM_ROTA,
+    ENTREGUE
+}
