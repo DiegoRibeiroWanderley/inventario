7fbb2bfbc5bffe550ea1792be778d4b8042f34f1
Diego <eliwandejp@gmail.com>
2026-03-17 21:06:03 -0300

    feat: Criar EntregaRepository para persistência

---
+++ src/main/java/com/inventario/projeto/repositories/EntregaRepository.java
@@ -0,0 +1,20 @@
+package com.inventario.projeto.repositories;
+
+import com.inventario.projeto.model.Entrega;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+@Repository
+public interface EntregaRepository extends JpaRepository<Entrega, Long> {
+    Page<Entrega> findEntregaByMovimentacao_Id(Integer movimentacaoId, Pageable paginacao);
+
+    Page<Entrega> findEntregaByMovimentacao_Item_Id(Integer itemId, Pageable paginacao);
+
+    @Query("select e from Entrega e where e.dataDespacho >= ?1 and e.dataDespacho <= ?2")
+    Page<Entrega> findEntregaByPeriodo(LocalDate inicio, LocalDate fim, Pageable paginacao);
+}
