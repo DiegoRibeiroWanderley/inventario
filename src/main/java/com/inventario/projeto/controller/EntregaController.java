c18ad127c956fc16d9de2127525046defb9e32f3
Diego <eliwandejp@gmail.com>
2026-03-18 20:12:01 -0300

    feat: Expor endpoints dos métodos adicionados para Entrega

--- src/main/java/com/inventario/projeto/controller/EntregaController.java
+++ src/main/java/com/inventario/projeto/controller/EntregaController.java
@@ -7,10 +7,7 @@ import com.inventario.projeto.service.EntregaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -31,5 +28,68 @@ public class EntregaController {
                     defaultValue = ParametrosDeBusca.ORDEM, required = false) String ordem) {
         Response<EntregaDTO> response = entregaService.findAll(numeroDaPagina, tamanhoDaPagina, ordenarEntregasPor, ordem);
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/entregas/movimentacao/{movimentacaoId}")
+    public ResponseEntity<Response<EntregaDTO>> findByMovimentacao(
+            @PathVariable Integer movimentacaoId,
+            @RequestParam(name = "numeroDaPagina",
+                    defaultValue = ParametrosDeBusca.NUMERO_DA_PAGINA, required = false) Integer numeroDaPagina,
+            @RequestParam(name = "tamanhoDaPagina",
+                    defaultValue = ParametrosDeBusca.TAMANHO_DA_PAGINA, required = false) Integer tamanhoDaPagina,
+            @RequestParam(name = "ordenarItemsPor",
+                    defaultValue = ParametrosDeBusca.ORDENAR_ENTREGAS_POR, required = false) String ordenarEntregasPor,
+            @RequestParam(name = "ordem",
+                    defaultValue = ParametrosDeBusca.ORDEM, required = false) String ordem) {
+        Response<EntregaDTO> response = entregaService.findByMovimentacao(numeroDaPagina, tamanhoDaPagina, ordenarEntregasPor, ordem, movimentacaoId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/entregas/item/{itemId}")
+    public ResponseEntity<Response<EntregaDTO>> findByItem(
+            @PathVariable Integer itemId,
+            @RequestParam(name = "numeroDaPagina",
+                    defaultValue = ParametrosDeBusca.NUMERO_DA_PAGINA, required = false) Integer numeroDaPagina,
+            @RequestParam(name = "tamanhoDaPagina",
+                    defaultValue = ParametrosDeBusca.TAMANHO_DA_PAGINA, required = false) Integer tamanhoDaPagina,
+            @RequestParam(name = "ordenarItemsPor",
+                    defaultValue = ParametrosDeBusca.ORDENAR_ENTREGAS_POR, required = false) String ordenarEntregasPor,
+            @RequestParam(name = "ordem",
+                    defaultValue = ParametrosDeBusca.ORDEM, required = false) String ordem) {
+        Response<EntregaDTO> response = entregaService.findByItem(numeroDaPagina, tamanhoDaPagina, ordenarEntregasPor, ordem, itemId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/entregas/{mesInicio}/{anoInicio}/{mesFinal}/{anoFinal}")
+    public ResponseEntity<Response<EntregaDTO>> findByItem(
+            @PathVariable String mesInicio, @PathVariable Integer anoInicio, @PathVariable String mesFinal, @PathVariable Integer anoFinal,
+            @RequestParam(name = "numeroDaPagina",
+                    defaultValue = ParametrosDeBusca.NUMERO_DA_PAGINA, required = false) Integer numeroDaPagina,
+            @RequestParam(name = "tamanhoDaPagina",
+                    defaultValue = ParametrosDeBusca.TAMANHO_DA_PAGINA, required = false) Integer tamanhoDaPagina,
+            @RequestParam(name = "ordenarItemsPor",
+                    defaultValue = ParametrosDeBusca.ORDENAR_ENTREGAS_POR, required = false) String ordenarEntregasPor,
+            @RequestParam(name = "ordem",
+                    defaultValue = ParametrosDeBusca.ORDEM, required = false) String ordem) {
+        Response<EntregaDTO> response = entregaService.findByPeriodo(numeroDaPagina, tamanhoDaPagina, ordenarEntregasPor, ordem, mesInicio, anoInicio, mesFinal, anoFinal);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/entrega/add/item/{itemId}/quantidade/{quantidade}")
+    public ResponseEntity<EntregaDTO> createEntrega(@RequestBody EntregaDTO entregaDTO, @PathVariable Integer itemId, @PathVariable Integer quantidade) {
+        EntregaDTO entrega = entregaService.createEntrega(entregaDTO, itemId, quantidade);
+        return new ResponseEntity<>(entrega, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/entrega/update/{entregaId}")
+    public ResponseEntity<EntregaDTO> updateEntrega(@RequestBody EntregaDTO entregaDTO, @PathVariable Integer entregaId) {
+        EntregaDTO updatedEntrega = entregaService.updateEntrega(entregaDTO, entregaId);
+        return new ResponseEntity<>(updatedEntrega, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/entrega/delete/{entregaId}")
+    public ResponseEntity<EntregaDTO> deleteEntrega(@PathVariable Integer entregaId) {
+        EntregaDTO deletedEntrega = entregaService.deleteEntrega(entregaId);
+        return new ResponseEntity<>(deletedEntrega, HttpStatus.OK);
     }
 }
