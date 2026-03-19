48b259d0a29d5a80ec3f870ead204dd5c17c3573
Diego <eliwandejp@gmail.com>
2026-03-19 20:05:41 -0300

    refactor: Criar outro construtor de NotFoundException com parâmetro String em vez de Integer

--- src/main/java/com/inventario/projeto/exception/NotFoundException.java
+++ src/main/java/com/inventario/projeto/exception/NotFoundException.java
@@ -4,4 +4,5 @@ public class NotFoundException extends RuntimeException {
     public NotFoundException(String name, Integer id) {
         super(String.format("%s with id %d not found", name, id));
     }
+    public NotFoundException(String name, String message) {super(String.format("%s not found, %s", name, message));}
 }
