66b4b37058db92b512d0eb49b9efb1f4a847f1f4
Diego <eliwandejp@gmail.com>
2026-03-22 16:15:33 -0300

    feat: Criar PedidoMapper para transformar Pedido em PedidoDTO e vice-versa

---
+++ src/main/java/com/inventario/projeto/mapper/PedidoMapper.java
@@ -0,0 +1,13 @@
+package com.inventario.projeto.mapper;
+
+import com.inventario.projeto.model.Pedido;
+import com.inventario.projeto.payload.DTO.PedidoDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = ItemMapper.class)
+public interface PedidoMapper {
+
+    PedidoDTO toPedidoDTO(Pedido pedido);
+    Pedido toPedido(PedidoDTO pedidoDTO);
+}
