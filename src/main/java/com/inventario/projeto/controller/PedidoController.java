b3694a6045ab552db9ae6c03f91d845d10fd30cc
Diego <eliwandejp@gmail.com>
2026-03-22 16:14:38 -0300

    feat: Expor endpoints de Pedido em PedidoController

---
+++ src/main/java/com/inventario/projeto/controller/PedidoController.java
@@ -0,0 +1,31 @@
+package com.inventario.projeto.controller;
+
+import com.inventario.projeto.payload.DTO.PedidoDTO;
+import com.inventario.projeto.service.PedidoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PedidoController {
+
+    private final PedidoService pedidoService;
+
+    @PostMapping("/pedido/item/{itemId}/qtde/{quantidade}")
+    public ResponseEntity<PedidoDTO> criarPedido(@PathVariable Integer itemId, @PathVariable Integer quantidade) {
+        PedidoDTO pedido = pedidoService.adicionarItemAoPedido(itemId, quantidade);
+        return new ResponseEntity<>(pedido, HttpStatus.OK);
+    }
+
+    @PostMapping("/pedido/lancar")
+    public ResponseEntity<PedidoDTO> lancarPedido() {
+        PedidoDTO pedido = pedidoService.lancarPedido();
+        return new ResponseEntity<>(pedido, HttpStatus.OK);
+    }
+}
