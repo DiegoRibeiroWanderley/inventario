6b7288a2d789af83d29aa073fa1462360505a2d8
Diego <eliwandejp@gmail.com>
2026-03-22 16:10:59 -0300

    feat: Criar entidades Pedido e ItemPedido e relaciona-las

---
+++ src/main/java/com/inventario/projeto/model/ItemPedido.java
@@ -0,0 +1,31 @@
+package com.inventario.projeto.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    @JsonIgnore
+    private Pedido pedido;
+
+    private Double taxaItem;
+    private Integer quantidade;
+    private Double precoDoItem;
+}
---
+++ src/main/java/com/inventario/projeto/model/Pedido.java
@@ -0,0 +1,30 @@
+package com.inventario.projeto.model;
+
+import com.inventario.projeto.model.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "pedido", cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    List<ItemPedido> itens;
+
+    private Double precoItems;
+    private Double precoTaxas;
+    private Double precoTotal;
+    private LocalDate dataPedido;
+    private Status status;
+}
