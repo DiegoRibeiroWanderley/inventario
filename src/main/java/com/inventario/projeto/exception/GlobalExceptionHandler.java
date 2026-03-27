c123f6b494fb41065ab06aff4595328f892dfd82
Diego <eliwandejp@gmail.com>
2026-03-27 18:33:59 -0300

    feat: Adicionar ExceptionHandler para a exceção MethodArgumentNotValidException

--- src/main/java/com/inventario/projeto/exception/GlobalExceptionHandler.java
+++ src/main/java/com/inventario/projeto/exception/GlobalExceptionHandler.java
@@ -3,11 +3,29 @@ package com.inventario.projeto.exception;
 import com.inventario.projeto.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> response = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String nomeDoCampo = ((FieldError)error).getField();
+            String mensagem = error.getDefaultMessage();
+            response.put(nomeDoCampo, mensagem);
+        });
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(APIException.class)
     public ResponseEntity<APIResponse> handleAPIException(APIException e) {
